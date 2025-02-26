7140abadc73ea56058153f06ea0ab739c4de4478
David Silva <d.silvas@outlook.com>
2025-02-26 07:19:30 +0000

    Initial commit

---
+++ session/api/src/test/java/dev/davidsilva/auth/session/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davidsilva.auth.session.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
